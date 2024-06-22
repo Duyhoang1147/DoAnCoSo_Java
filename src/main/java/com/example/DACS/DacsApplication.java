e9d80e50cf11baf095e67f51b82752965c412ce7
Duy Hoang <Duyhoang1147@gmail.com>
2024-06-22 16:09:03 +0700

    Create Project

---
+++ src/main/java/com/example/DACS/DacsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DACS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacsApplication.class, args);
+	}
+
+}

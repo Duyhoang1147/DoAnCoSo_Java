0d0bee739f52d0d1e2083d3dd327f2e2277a2291
Duy Hoang <Duyhoang1147@gmail.com>
2024-06-26 23:41:52 +0700

    Add RequestMapping for all Controller

--- src/main/java/com/example/DACS/Controller/DeliveryInvoiceController.java
+++ src/main/java/com/example/DACS/Controller/DeliveryInvoiceController.java
@@ -1,7 +1,9 @@
 package com.example.DACS.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/deliveryIvoices")
 public class DeliveryInvoiceController {
 }

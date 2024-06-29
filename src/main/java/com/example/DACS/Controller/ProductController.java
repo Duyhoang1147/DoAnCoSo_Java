3f2534c57690b749b8278d7132df6a398b7ec3df
Duy Hoang <Duyhoang1147@gmail.com>
2024-06-29 17:22:55 +0700

    Add Add_product

--- src/main/java/com/example/DACS/Controller/ProductController.java
+++ src/main/java/com/example/DACS/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.DACS.Controller;
 import com.example.DACS.Model.Product;
 import com.example.DACS.Service.CategoryService;
 import com.example.DACS.Service.ProductService;
+import com.example.DACS.Service.SupplierSerVice;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.parameters.P;
@@ -23,6 +24,9 @@ public class ProductController {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    SupplierSerVice supplierSerVice;
+
     @GetMapping
     public String showForm(Model model)
     {
@@ -35,13 +39,18 @@ public class ProductController {
     {
         model.addAttribute("product", new Product());
         model.addAttribute("category", categoryService.getAllCategories());
+        model.addAttribute("supplier", supplierSerVice.getAllSupplier());
         return "product/add_product";
     }
 
     @PostMapping("/add")
-    public String addProduct(@Valid Product product, BindingResult error)
+    public String addProduct(@Valid Product product, BindingResult error, Model model)
     {
-        if(error.hasErrors()) { return "product/add_product"; }
+        if(error.hasErrors())
+        {
+            model.addAttribute("product", product);
+            return "product/add_product";
+        }
         productService.addProduct(product);
         return "redirect:/products";
     }
